8db7beee8292cf8de230405107c16763324d6e26
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2023-06-22 10:33:25 -0300

    test

---
+++ backlog/src/main/java/co/a3tecnology/backlog/DebugLog.java
@@ -0,0 +1,10 @@
+package co.a3tecnology.backlog;
+
+import android.util.Log;
+
+public final class DebugLog {
+
+    public static void print(String message) {
+        Log.d("DebugLog", message);
+    }
+}
---
+++ crashlog/src/main/java/co/a3tecnology/crashlog/CrashLog.java
@@ -0,0 +1,10 @@
+package co.a3tecnology.crashlog;
+
+import android.util.Log;
+
+public final class CrashLog {
+
+    public static void print(String message) {
+        Log.d("Debugloh", message);
+    }
+}
